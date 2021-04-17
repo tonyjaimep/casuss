dd549fa40051d60c0c3076bad55a94be5a5052ee
tonyjaimep <tonyjaimep@gmail.com>
2021-04-16 21:12:15 -0500

    Add basic homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import Homepage from './views/Homepage'
+
+const App = () => {
+  return <main>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true} component={Homepage}/>
+      </Switch>
+    </Router>
+  </main>
 }
 
 export default App;
---
+++ src/views/Homepage.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import Input from '../components/Input.jsx'
+
+import { Search } from 'react-feather'
+
+import { useHistory } from 'react-router-dom'
+
+import {
+  Form,
+  Formik,
+} from 'formik'
+
+const Homepage = () => {
+  const history = useHistory()
+
+  const formikConfig = {
+    initialValues: { query: "" },
+    onSubmit: values => {
+      history.push({
+        pathname: '/search',
+        search: `?q=${values.query}`
+      })
+    }
+  }
+
+  return <section>
+    <div className="container">
+      <h1 className="text-4xl font-bold mb-4">Encuentro mis libros favoritos</h1>
+      <Formik {...formikConfig}>
+        <Form>
+          <div className="relative">
+            <Input type="text" name="query" placeholder="Buscar por título, autor o descripción"/>
+            <button className="absolute right-0 top-0 p-4" type="submit">
+              <Search size="1.4em"/>
+            </button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  </section>
+}
+
+export default Homepage
